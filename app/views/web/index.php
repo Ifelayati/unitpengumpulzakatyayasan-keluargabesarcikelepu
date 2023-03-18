9feec97b46d9119a808f9b2cf7d99ff2278adfa9
muhammad iqbal felayai <90131734+Ifelayati@users.noreply.github.com>
2023-03-18 17:49:35 +0700

    edit tampilan

--- app/views/web/index.php
+++ app/views/web/index.php
@@ -63,20 +63,9 @@
                         <div class="section_title">
                             <h3><span>Apa itu Zakat </span> 
                                 Mal ?</h3>
-                            <p>Zakat maal yang dimaksud dalam perhitungan ini adalah zakat yang dikenakan atas uang, emas, surat berharga, dan aset yang disewakan (Al Qur'an Surah At Taubah ayat 103, Peraturan Menteri Agama No 52/2014 dan pendapat Shaikh Yusuf Qardawi). Tidak termasuk harta pertanian, pertambangan, dan lain-lain yang diatur dalam UU No.23/2011 tentang pengelolaan zakat. Zakat maal harus sudah mencapai nishab (batas minimum) dan terbebas dari hutang serta kepemilikan telah mencapai 1 tahun (haul).
+                            <p>Zakat mal yang dimaksud dalam perhitungan ini adalah zakat yang dikenakan atas uang, emas, surat berharga, dan aset yang disewakan (Al Qur'an Surah At Taubah ayat 103, Peraturan Menteri Agama No 52/2014 dan pendapat Shaikh Yusuf Qardawi). Tidak termasuk harta pertanian, pertambangan, dan lain-lain yang diatur dalam UU No.23/2011 tentang pengelolaan zakat. Zakat maal harus sudah mencapai nishab (batas minimum) dan terbebas dari hutang serta kepemilikan telah mencapai 1 tahun (haul).
                             </p>
-
-                            <p>
-                            Standar harga emas yg digunakan untuk 1 gram nya adalah Rp600.000,-. Sementara nishab yang digunakan adalah sebesar 85 gram emas.
-                            <br>
-                            Adapun cara menghitung zakat maal sebagai berikut:
-                            2,5% x Jumlah harta yang tersimpan selama 1 tahun
-                            <br>
-                            Contoh:
-                            <br>
-                            Bapak A selama 1 tahun penuh memiliki harta yang tersimpan (emas/perak/uang) senilai Rp100.000.000,-. Jika harga emas saat ini Rp600.000,-/gram, maka nishab zakat senilai Rp51.000.000,-. Sehingga Bapak A sudah wajib zakat. Zakat maal yang perlu Bapak A tunaikan sebesar 2,5% x Rp100.000.000,- = Rp2.500.000,-.
-                            </p>
-                            <a href="<?=BASEURL;?>/Web/donasi" class="boxed-btn3">Donasi Sekarang</a>
+                           
                         </div>
                     </div>
                 </div>
@@ -96,25 +85,16 @@
             <div class="row align-items-center">
                 <div class="col-lg-5 col-md-6">
                     <div class="pet_thumb">
-                        <img src="<?= BASEURL; ?>/assets_web/img/about/zakat_fitrah.jpg" alt="">
+                        <img src="<?= BASEURL; ?>/assets_web/img/about/ternak.png" alt="">
                     </div>
                 </div>
                 <div class="col-lg-6 offset-lg-1 col-md-6">
                     <div class="pet_info">
                         <div class="section_title">
-                            <h3><span>Apa itu Zakat </span> 
-                                Fitrah ?</h3>
-                            <p>
-                            Zakat fitrah merupakan salah satu dari jenis zakat yang wajib dikeluarkan setiap individu merdeka dan mampu serta sesuai dengan syarat yang telah ditetapkan. Zakat sendiri telah menjadi salah satu bagian dari rukun islam yang ke-4. Oleh karena itu, diwajibkan kita sebagai umat muslim untuk selalu membayar zakat terutama zakat fitrah.
+                        <h4> <span>Zakat Peternakan </span>
+                                </h4>
+                            <p>Hewan peternakan yang wajib dikeluarkan zakatnya menurut sebagian ulama ada 3,yaitu Unta,Sapi/kerbau dan Kambing/domba dengan nisab masing-masing tiap hewan berbeda dan nilai zakat yang dikeluarkannya pun Berbeda,Tapi untuk haulnya sama yaitu 1 tahun kepemilikan. <br> Berikut penjelasannya
                             </p>
-                            <p>
-                            Pengertian dari zakat fitrah sendiri yaitu zakat yang berguna untuk membersihkan harta dan sebagai pelengkap ibadah puasa kita. Tanpa zakat fitrah, puasa kita tidak terlengkapi.</p>
-                            <p>                       
-                            عَنْ ابْنِ عُمَرَ رَضِيَ اللهُ عَنْهُمَا أَنَّ رَسُوْلَ اللهِ صَلَّى الله عليه وسلم    قَالَ : أُمِرْتُ أَنْ أُقَاتِلَ النَّاسَ حَتَّى يَشْهَدُوا أَنْ لاَ إِلَهَ إِلاَّ اللهُ وَأَنَّ مُحَمَّداً رَسُوْلُ اللهِ، وَيُقِيْمُوا الصَّلاَةَ وَيُؤْتُوا الزَّكاَةَ، فَإِذَا فَعَلُوا ذَلِكَ عَصَمُوا  مِنِّي دِمَاءُهُمْ وَأَمْوَالُـهُمْ إِلاَّ بِحَقِّ الإِسْلاَمِ وَحِسَابُهُمْ عَلَى اللهِ تَعَالىَ
-                            <br>
-                            “Dari Ibnu Umar radhiallahuanhuma sesungguhnya Rasulullah Shallallahu’alaihi wasallam bersabda: Aku diperintahkan untuk memerangi manusia hingga mereka bersaksi bahwa tidak ada Ilah selain Allah dan bahwa Muhammad adalah Rasulullah, menegakkan shalat, menunaikan zakat. Jika mereka melakukan hal itu maka darah dan harta  mereka akan dilindungi kecuali dengan hak Islam dan perhitungan mereka ada pada Allah Subhanahu wata’ala.” (HR. Bukhari no. 25; Muslim no. 22)
-                            </p>
-                            <a href="<?=BASEURL;?>/Web/donasi" class="boxed-btn3">Donasi Sekarang</a>
                         </div>
                     </div>
                 </div>
@@ -123,29 +103,64 @@
     </div>
     <!-- pet_care_area_end  -->
 
-        <!-- pet_care_area_start  -->
+<!--  awal table pertanian  -->
+<div class=" p-5">
+<table class="table  table-bordered border-primary ">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Jenis</th>
+      <th scope="col">Nisab/Haul</th>
+      <th scope="col">Zakat yang dibayarkan</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Unta</td>
+      <td>5 ekor /1tahun</td>
+      <td>1 ekor Kambing/Domba (atau bisa diuangkan)</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Sapi</td>
+      <td>30 ekor/1tahun</td>
+      <td>1 ekor anak sapi (atau bisa diuangkan) </td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td >Kambing/Domba</td>
+      <td>40 ekor/1tahun</td>
+      <td>1 ekor anak kambing (atau bisa diuangkan)</td>
+    </tr>
+  </tbody>
+</table>
+<div class="container px-4 text-center">
+  <div class="row gx-5">
+    <div class="col">
+     <div class="p-3"> <a href="<?=BASEURL;?>/Web/donasi" class="boxed-btn3">Bayar Zakat </a></div>
+    </div>
+    <div class="col">
+      <div class="p-3"><a href="<?=BASEURL;?>/Web/donasi" class="boxed-btn3">Konsultasi Zakat</a></div>
+    </div>
+  </div>
+</div>
+</div>
+<!--  akhir table pertanian  -->
+
+
+    <!-- pet_care_area_start  -->
     <div class="pet_care_area">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6 offset-lg-1 col-md-6">
                     <div class="pet_info">
                         <div class="section_title">
-                            <h3><span>Apa itu Zakat </span> 
-                                Mal ?</h3>
-                            <p>Zakat maal yang dimaksud dalam perhitungan ini adalah zakat yang dikenakan atas uang, emas, surat berharga, dan aset yang disewakan (Al Qur'an Surah At Taubah ayat 103, Peraturan Menteri Agama No 52/2014 dan pendapat Shaikh Yusuf Qardawi). Tidak termasuk harta pertanian, pertambangan, dan lain-lain yang diatur dalam UU No.23/2011 tentang pengelolaan zakat. Zakat maal harus sudah mencapai nishab (batas minimum) dan terbebas dari hutang serta kepemilikan telah mencapai 1 tahun (haul).
+                            <h4> <span> Zakat Pertanian </span> </h4>
+                            <p>Zakat pertanian adalah zakat yang wajib dikeluarkan bagi para petani,tetapi tidak semua buah hasil pertanian harus dikeluarkan zakatnya,menurut sebagain ulama ada 3 jenis hasil pertanian yang wajib dikeluarkan zakatnya diantaranya<br>Kurma,Anggur dan Hasil pertanian yang menjadi makanan poko 
+                            
                             </p>
-
-                            <p>
-                            Standar harga emas yg digunakan untuk 1 gram nya adalah Rp600.000,-. Sementara nishab yang digunakan adalah sebesar 85 gram emas.
-                            <br>
-                            Adapun cara menghitung zakat maal sebagai berikut:
-                            2,5% x Jumlah harta yang tersimpan selama 1 tahun
-                            <br>
-                            Contoh:
-                            <br>
-                            Bapak A selama 1 tahun penuh memiliki harta yang tersimpan (emas/perak/uang) senilai Rp100.000.000,-. Jika harga emas saat ini Rp600.000,-/gram, maka nishab zakat senilai Rp51.000.000,-. Sehingga Bapak A sudah wajib zakat. Zakat maal yang perlu Bapak A tunaikan sebesar 2,5% x Rp100.000.000,- = Rp2.500.000,-.
-                            </p>
-                            <a href="<?=BASEURL;?>/Web/donasi" class="boxed-btn3">Donasi Sekarang</a>
+                           
                         </div>
                     </div>
                 </div>
